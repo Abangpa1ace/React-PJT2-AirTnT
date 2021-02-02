86215d7925528c218c1286d6ced1b908747993c0
Abangpa1ace <dyek72731@gmail.com>
2021-02-02 23:47:27 +0900

    Add: Signin/Signup 로직 구현중

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require('bcrypt');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+let usersDB = [];
+
+router.get('/', (req, res, next) => {
+  res.render('users')
+})
+
+router.post('/signup', async (req, res, next) => {
+  // try {
+  //   const { firstName, lastName, email, password } = req.body;
+  //   const hashedPassword = await bcrypt.hash(password, 10);
+  //   usersDB.push({
+  //     id: Date.now().toString(),
+  //     firstName: firstName,
+  //     lastName: lastName,
+  //     email: email,
+  //     password: hashedPassword,
+  //   })
+  // }
+  // catch(err) {
+  //   throw new Error(err);
+  // }
+  console.log(usersDB)
+})
+
+router.post('/signin', async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const userMatched = usersDB.find((userData) => email === userData.email);
+    if (!userMatched) {
+      console.log(usersDB, "Not Registered User")
+    }
+    else {
+      console.log("success");
+    }
+    next();
+  }
+  catch(err) {
+    throw new Error(err);
+  }
+})
 
 module.exports = router;
