823a69bb34c0d33c73e4f800239181506560bbb4
Abangpa1ace <dyek72731@gmail.com>
2021-02-05 17:27:45 +0900

    Fix: Main Merge(List 페이지)

--- client/src/pages/List/List.js
+++ client/src/pages/List/List.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components';
 import { useGlobalContext } from '../../Context'
-import ListRests from './ListRests';
-import ListMap from './ListMap';
+import ListRests from './ListRests/ListRests';
+import ListMap from './ListMap/ListMap';
 
 const List = () => {
   const [restId, setRestId] = useState(-1);
